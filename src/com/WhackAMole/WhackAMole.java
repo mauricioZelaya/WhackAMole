072a2b7c3e9574751555f9580afaf54e2b820e08
Mauricio <mauricio.zelaya@jalasoft.com>
2018-04-09 19:17:19 -0700

    First Approach draft 2

--- src/com/WhackAMole/WhackAMole.java
+++ src/com/WhackAMole/WhackAMole.java
@@ -9,6 +9,8 @@ public class WhackAMole {
     public WhackAMole(int numAttempts, int gridDimension){
         attemptsLeft = numAttempts;
         moleGrid = new char[gridDimension][gridDimension];
+        initializeMoleGrid();
+        placeMolesInTheGrid();
     }
 
     public boolean place (int x, int y) {
@@ -18,10 +20,32 @@ public class WhackAMole {
 
     public void printGridToUSer() {
         for (int i = 0; i < moleGrid.length; i++) {
-            System.out.println("*");
-//            for (int j = 0; j < moleGrid.length; j++) {
-//                System.out.println('*');
-//            }
+            for (int j = 0; j < moleGrid[i].length; j++) {
+                System.out.println(moleGrid[i][j] + " ");
+            }
+            System.out.println();
         }
+    }
+
+    private void initializeMoleGrid(){
+        for (int i = 0; i < moleGrid.length; i++) {
+            for (int j = 0; j < moleGrid.length; j++) {
+                moleGrid[i][j] = '*';
+            }
+        }
+    }
+
+    private void placeMolesInTheGrid(){
+        int xCordinate;
+        int yCordinate;
+        for (int i = 0; i < 10; i++){
+            xCordinate = generateRandomPosition();
+            yCordinate = generateRandomPosition();
+            moleGrid[xCordinate][yCordinate] = 'M';
+        }
+    }
+
+    private int generateRandomPosition(){
+        return (int)Math.random() * 9;
     }
 }
